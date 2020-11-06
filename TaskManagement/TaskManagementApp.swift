0f2ff9b528cbe6e81c1577114921264fba2e03c8
Samuel F. Ademola <zion@Ebenezer.local>
2020-11-06 17:17:20 -0600

    init commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Samuel F. Ademola on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
